639f5cb3e635c7611d86d0b5f5d9da59da9ec10d
Maxime Florile <florile.maxime@gmail.com>
2016-02-19 11:44:06 +0100

    Initial Commit

---
+++ Projet3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projet3
+//
+//  Created by Maxime Florile on 19/02/2016.
+//  Copyright © 2016 Maxime Florile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
